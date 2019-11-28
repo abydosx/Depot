784cc978dbda525ff78a3e948d367cd8c53c09ac
ishidaxie <caorya@qq.com>
2019-11-28 15:58:26 +0800

    finish email

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,26 @@
+class OrderMailer < ApplicationMailer
+  default from: 'Sam Ruby <depot@example.com>'
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: order.email, subject: '在线书城订单确认'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+
+    mail to: order.email, subject: '您的商品已发货'
+  end
+
+
+end
